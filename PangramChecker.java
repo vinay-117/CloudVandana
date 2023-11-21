aa130a6b74c32d6bc1d3b849d3ed747d291f4889
Vinay Maddimsetti <123378117+vinay-117@users.noreply.github.com>
2023-11-21 10:43:36 +0530

    Add files via upload

---
+++ PangramChecker.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class PangramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence to check if it's a pangram: ");
+        String input = scanner.nextLine().toLowerCase(); // Convert to lowercase for case-insensitivity
+
+        boolean isPangram = checkPangram(input);
+
+        if (isPangram) {
+            System.out.println("The input is a pangram!");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean checkPangram(String input) {
+        Set<Character> uniqueLetters = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                uniqueLetters.add(c);
+            }
+        }
+
+        return uniqueLetters.size() == 26; // There are 26 letters in the English alphabet
+    }
+}
